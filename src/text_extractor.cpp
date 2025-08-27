a3c73219910a7e2724efefd32525788d79dbf1e1
Tom Keidar <tom.keidar@gmail.com>
2025-08-28 01:25:08 +0300

    update: use old style of null object creation instead of convoluted version detection

--- src/text_extractor.cpp
+++ src/text_extractor.cpp
@@ -101,25 +101,10 @@ extern "C" {
             
             // Create a null object for the MemStream dictionary.
             // The API for this has changed multiple times in poppler's history.
-            // Workaround for inconsistent poppler packaging in Debian Trixie.
-            // Version 25.03.0 reports a new version number but contains old headers.
-            #if (POPPLER_VERSION_MAJOR == 25 && POPPLER_VERSION_MINOR == 3)
-                // SPECIAL CASE: Trixie's 25.03 uses the 22.04-24.03 API.
-                BaseStream *stream = new MemStream((char*)data, 0, length, Object(objNull));
-            #elif (POPPLER_VERSION_MAJOR > 24) || (POPPLER_VERSION_MAJOR == 24 && POPPLER_VERSION_MINOR >= 4)
-                // Newest API (24.04+): Object::null() was re-introduced.
-                BaseStream *stream = new MemStream((char*)data, 0, length, Object::null());
-            #elif (POPPLER_VERSION_MAJOR > 22) || (POPPLER_VERSION_MAJOR == 22 && POPPLER_VERSION_MINOR >= 4)
-                // API from 22.04 to 24.03: Use Object(Object::nullObj).
-                BaseStream *stream = new MemStream((char*)data, 0, length, Object(objNull));
-            #elif (POPPLER_VERSION_MAJOR > 22) || (POPPLER_VERSION_MAJOR == 22 && POPPLER_VERSION_MINOR >= 1)
-                // API from 22.01 to 22.03: Use the default constructor.
-                BaseStream *stream = new MemStream((char*)data, 0, length, Object());
-            #else
-                // Old API (< 22.01): Use the original Object::null().
-                BaseStream *stream = new MemStream((char*)data, 0, length, Object::null());
-            #endif
-
+            Object nullDict;
+            nullDict.setToNull();
+            BaseStream *stream = new MemStream((char*)data, 0, length, std::move(nullDict));
+     
             // The PDFDoc constructor for streams also takes std::optional for passwords.
             std::unique_ptr<PDFDoc> doc(new PDFDoc(stream, user_pw, {}));
 
